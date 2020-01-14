3acf509f62a9bc99a45d1c9ef308424d40b236b0
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-14 15:29:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,15 +7,16 @@ end
 def find_max_value(array)
   # Add your solution here
   #array.max()
+  max = nil
+  array.each { |element|
+     max = element if element > max
+    }
+    max
 
 end
 
 def find_min_value(array)
   # Add your solution here
   #array.min()
-  max = nil
-  array.each { |element|
-     max = element if element > max
-    }
-    max
+  
 end
